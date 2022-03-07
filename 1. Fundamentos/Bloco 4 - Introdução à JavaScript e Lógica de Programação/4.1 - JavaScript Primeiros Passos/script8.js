f3386a8b1ef3814161cec6b43648f95979ed9176
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-03-07 19:15:58 -0300

    finaliza exercicio 8

---
+++ 1. Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.1 - JavaScript Primeiros Passos/script8.js
@@ -0,0 +1,11 @@
+const primeiro = -1;
+const segundo = 10;
+const terceiro = 13;
+
+if (primeiro % 2 == 0 || segundo % 2 == 0 || terceiro % 2 == 0) {
+    console.log(true);
+}
+
+else {
+    console.log(false);
+}
