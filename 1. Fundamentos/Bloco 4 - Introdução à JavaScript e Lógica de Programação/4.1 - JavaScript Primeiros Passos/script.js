5842f1b678d6098c2971f123ff2f722389db3917
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-03-07 15:14:05 -0300

    finaliza questão 1

---
+++ 1. Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.1 - JavaScript Primeiros Passos/script.js
@@ -0,0 +1,8 @@
+const a = 9;
+const b = 3;
+
+console.log("O resultado da soma é " + (a+b));
+console.log("O resultado da subtração é " + (a-b));
+console.log("O resultado da multiplicação é " + (a*b));
+console.log("O resultado da divisão é " + (a/b));
+console.log("O resultado do resto da divisão é " + (a%b));
