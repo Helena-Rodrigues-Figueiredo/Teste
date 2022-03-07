bb7f9ed545368f9a2d7b8dac4587c056b5a25716
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-03-07 16:09:27 -0300

    finaliza exercicio 4

--- 1. Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.1 - JavaScript Primeiros Passos/script2.js
+++ 1. Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.1 - JavaScript Primeiros Passos/script2.js
@@ -2,9 +2,9 @@ const primeiro = 10;
 const segundo = 20;
 
 if (primeiro > segundo) {
-    console.log("O número maior é o " + primeiro)
+    console.log("O número maior é o " + primeiro);
 }
 
 else {
-    console.log("O número maior é o " + segundo)
+    console.log("O número maior é o " + segundo);
 }
