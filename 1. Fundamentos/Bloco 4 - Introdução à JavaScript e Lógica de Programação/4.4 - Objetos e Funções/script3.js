95ed3583717e64dbe315113b11bc15030c3877eb
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-03-11 10:16:18 -0300

    finaliza exercício 3

---
+++ 1. Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.4 - Objetos e Funções/script3.js
@@ -0,0 +1,11 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+//   3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+
+for (index in info) {
+    console.log(index);
+}
