195e782b6b4cb264007d3e3ae02245dbe6443359
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-03-17 11:03:59 -0300

    finaliza exercícios

--- 1. Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.1 Dom e Seletores/script.js
+++
@@ -1,8 +0,0 @@
-const emergencyTasks = document.getElementsByClassName('emergency-tasks')[0];
-emergencyTasks.style.backgroundColor = 'rgb(255, 159, 132)';
-
-const noEmergencyTasks = document.getElementsByClassName('no-emergency-tasks')[0];
-noEmergencyTasks.style.backgroundColor = 'rgb(249, 219, 94)';
-
-const headerContainer = document.getElementById('header-container');
-headerContainer.style.backgroundColor = 'rgb(0, 176, 105)';
