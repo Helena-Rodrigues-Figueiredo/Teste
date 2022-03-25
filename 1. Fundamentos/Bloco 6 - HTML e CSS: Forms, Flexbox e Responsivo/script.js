c1e25e25ba04bd5748c82f9c823a1a91514109aa
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-03-25 12:39:16 -0300

    inicia questão bônus 1

--- 1. Fundamentos/Bloco 6 - HTML e CSS: Forms, Flexbox e Responsivo/script.js
+++ 1. Fundamentos/Bloco 6 - HTML e CSS: Forms, Flexbox e Responsivo/script.js
@@ -2,6 +2,7 @@ const btnEnviar = document.getElementById('envia-informacoes');
 const btnApagar = document.getElementById('apaga-informacoes');
 const corpoForm = document.querySelectorAll('input');
 const marcarCheck = document.getElementById('concordo1');
+const checado = document.getElementById('cordordo2');
 
 function interrompeComportamento(event) {
     event.preventDefault();
@@ -17,7 +18,9 @@ btnApagar.addEventListener('click', apagaDados);
 
 
 function habilitaBotao() {
-    if () {
-        btnEnviar
+    if(checado.checked === true) {
+        btnEnviar.disabled = false;
     }
 }
+
+checado.addEventListener('change', habilitaBotao);
